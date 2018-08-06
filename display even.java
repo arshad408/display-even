7496e9822033a31e2c507601fbd343e79e97bce7
arshad408 <arshadachu408@gmail.com>
2018-08-06 14:32:26 +0530

    Create display even.java

---
+++ display even.java
@@ -0,0 +1,15 @@
+# display-even
+class even
+{
+int n;
+Scanner s=new Scanner(System.in);
+n=s.nextLine();
+public Static void main(String args[])
+{
+System.out.println("The even nums are");
+for (int i=1;i<=n;i++)
+{
+if(i%2==0)
+{
+System.out.println(i+"");
+}}}}
